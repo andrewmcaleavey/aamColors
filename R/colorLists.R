8ab43ffa0ccc24dc64a52c1554953705741f025b
Andrew McAleavey <andrew.mcaleavey@gmail.com>
2024-09-07 11:06:36 +0200

    making into a package

---
+++ R/colorLists.R
@@ -0,0 +1,6 @@
+all_aam_colours = list(
+  summer = c("#086B08", "#9BE629", "#19088A", "#F7633E", "#F5A1D9", 
+             "#C9E5FF", "#EBF7FA", "#F7EEDC", "#DB9A51", "#6B6B6B"),
+  winter = c("#116B0A", "#9E7928", "#2972AD", "#A8A5A5", "#CDE8FA", 
+             "#18A1F0", "#FFFEED")
+)
---
+++ R/palette.R
@@ -0,0 +1,15 @@
+aam_palettes = function(name, 
+                        n, 
+                        all_palettes = all_aam_colours, 
+                        type = c("discrete", "continuous")) {
+  palette = all_palettes[[name]]
+  if (missing(n)) {
+    n = length(palette)
+  }
+  type = match.arg(type)
+  out = switch(type,
+               continuous = grDevices::colorRampPalette(palette)(n),
+               discrete = palette[1:n]
+  )
+  structure(out, name = name, class = "palette")
+}
